a728d78077386924dc525ec5e018f142fce93641
kissakos <nekemisvan@gmail.com>
2020-12-27 01:52:04 +0100

    8

---
+++ test_dp_adapter/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test_dp_adapter
+    {
+    class Class1
+        {
+        public static void hello()
+            { Console.WriteLine("Hello World! 8"); }
+        }
+    }
