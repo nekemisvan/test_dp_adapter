aadcb8e03b6f416357c670051e64d2a9aeaa8b67
kissakos <nekemisvan@gmail.com>
2020-12-27 02:08:23 +0100

    9

--- test_dp_adapter/Class1.cs
+++ test_dp_adapter/Class1.cs
@@ -7,6 +7,6 @@ namespace test_dp_adapter
     class Class1
         {
         public static void hello()
-            { Console.WriteLine("Hello World! 8"); }
+            { Console.WriteLine("Hello World! 9"); }
         }
     }
