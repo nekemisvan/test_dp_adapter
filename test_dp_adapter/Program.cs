8e706d266e54d6514285cc74c1a48595df1d718d
kissakos <nekemisvan@gmail.com>
2021-01-03 03:18:37 +0100

    9b

--- test_dp_adapter/Program.cs
+++ test_dp_adapter/Program.cs
@@ -14,7 +14,7 @@ namespace test_dp_adapter
 
             foreach (IEmployee i in list) i.ShowHappiness();
 
-            Console.WriteLine("Hello World! 9");  
+            Console.WriteLine("Hello World! 9b");  
             }
 
 
